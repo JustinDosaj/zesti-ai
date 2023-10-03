652f1d3e6eb4b1d092f88e29d7051d78381130d1
Justin Dosaj <jdosaj@vurge.io>
2023-10-03 14:38:59 -0700

    Updates
    1. Added scraper api
    2. Parser added to parse scarpped data
    3. raw data is uploaded to firestore to be used in cloud functions

--- src/pages/api/handler/submit.tsx
+++ src/pages/api/handler/submit.tsx
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebase";
+import { Scrape } from "../scrape/scraper";
 
 export interface Props {
     url: any,
@@ -7,16 +8,24 @@ export interface Props {
 
 export const handleSubmit = async ({url, user}: Props): Promise<void> => {
 
+    const name = await cleanUrl(url)
+    const response = await Scrape(url)
     const obj = {
         "url": `${url}`,
-        "name": "placeholder",
+        "rawData": response,
     }
     
     if(user){
         try {
-            await db.collection('users').doc(user.uid).collection('pages').doc(url).set(obj)
+            await db.collection('users').doc(user.uid).collection('pages').doc(name).set(obj)
         } catch (err) {
             console.log(err)
         }
     }
+}
+
+export const cleanUrl = async (url: any) => {
+    let name = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '')
+    name = name.split('/')[0];
+    return name
 }
