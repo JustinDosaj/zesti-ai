ee7f97e23a8afa17d8c992d9bedfb8fd7a0aacaa
Justin Dosaj <jdosaj@vurge.io>
2023-10-23 16:15:15 -0700

    UI Updates
    Changed Icon Source

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,6 +3,7 @@ import { Paragraph } from "@/components/shared/paragraph"
 import { Container } from "@/components/shared/container"
 import { Button } from "@/components/shared/button"
 import { Raleway } from 'next/font/google'
+import { EnvelopeIcon, MapPinIcon, UsersIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline"
 import Head from 'next/head';
 
 
@@ -15,40 +16,21 @@ export default function Contact() {
         <section className="relative my-auto ">
             <Container className={"grid md:grid-cols-2 lg:flex-row gap-10 lg:gap-12"}>
                 <div className="flex-1 flex flex-col">
-                    <Title>
+                    <Title className="text-black">
                         Contact Us
                     </Title>
-                    <Paragraph className="mt-8">
+                    <Paragraph className="mt-8 text-gray-600">
                         Send us a message and we will respond to you as soon as possible!                
                     </Paragraph>
-                    <Paragraph className="mt-2 inline-flex">
+                    <Paragraph className="mt-2 inline-flex text-gray-600">
                         <span className="min-w-max pr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
-                                <path d="M3 7l9 6l9 -6"></path>
-                            </svg>                                                                
+                            <EnvelopeIcon className="h-6 w-6 text-black"/>                                                                 
                         </span>
                         jdosaj@vurge.io
                     </Paragraph>
-                    <Paragraph className="mt-2 inline-flex">
+                    <Paragraph className="mt-2 inline-flex text-gray-600">
                         <span className="min-w-max pr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-                            </svg>                                                                 
-                        </span>
-                        (805) 206-1623
-                    </Paragraph>
-                    <Paragraph className="mt-2 inline-flex">
-                        <span className="min-w-max pr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pins" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M10.828 9.828a4 4 0 1 0 -5.656 0l2.828 2.829l2.828 -2.829z"></path>
-                                <path d="M8 7l0 .01"></path>
-                                <path d="M18.828 17.828a4 4 0 1 0 -5.656 0l2.828 2.829l2.828 -2.829z"></path>
-                                <path d="M16 15l0 .01"></path>
-                            </svg>                                                               
+                            <MapPinIcon className="h-6 w-6 text-black"/>                                                               
                         </span>
                         3400 Cottage Way, Ste G2 #19004 
                         Sacramento, California 95825 Us
@@ -57,23 +39,13 @@ export default function Contact() {
                 <form action="https://formspree.io/f/maygvonw" method="POST" target="_blank">
                 <div className="py-1 pl-6 w-full pr-1 flex gap-3 items-center text-heading-3 shadow-lg shadow-box-shadow border border-box-border bg-box-bg rounded-full ease-linear focus-within:bg-body  focus-within:border-primary">
                     <span className="min-w-max pr-2 border-r border-box-border">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-                            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                            <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
-                         </svg>                                                                
+                        <UsersIcon className="h-6 w-6 text-black"/>                                                             
                     </span>
                     <input type="text" name="FULLNAME" placeholder="Name" className="w-full py-3 outline-none bg-transparent text"/>
                         </div>
                         <div className="mt-4 py-1 pl-6 w-full pr-1 flex gap-3 items-center text-heading-3 shadow-lg shadow-box-shadow border border-box-border bg-box-bg rounded-full ease-linear focus-within:bg-body  focus-within:border-primary">
                             <span className="min-w-max pr-2 border-r border-box-border">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
-                                    <path d="M3 7l9 6l9 -6"></path>
-                                </svg>                                                                 
+                                <EnvelopeIcon className="h-6 w-6 text-black"/>                                                                 
                             </span>
                             <input type="email" name="EMAIL" placeholder="Email" className="w-full py-3 outline-none bg-transparent required email"/>
                         </div>
@@ -86,9 +58,7 @@ export default function Contact() {
                                     Submit
                                 </span>
                                 <span className="flex sm:hidden relative z-[5]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                                    </svg>                                      
+                                    <PaperAirplaneIcon className="h-6 w-6 text-white"/>                                     
                                 </span>
                             </Button>
                         </div>
