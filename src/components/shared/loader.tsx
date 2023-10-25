e97256b01aaa16ea9bd5e846678035bb6d48ca7d
Justin Dosaj <jdosaj@vurge.io>
2023-10-24 18:31:53 -0700

    Final Update before launch

--- src/components/shared/loader.tsx
+++ src/components/shared/loader.tsx
@@ -3,7 +3,7 @@
 export function Loader() {
 
     return(
-    <div className=" bg-white px-6 py-3 rounded-full outline-none relative overflow-hidden border duration-300 ease-linear border-primary-main hover:cursor-not-allowed">
+    <div className="grid justify-center bg-white px-6 py-3 rounded-full outline-none relative overflow-hidden border duration-300 ease-linear border-primary-main hover:cursor-not-allowed">
         <div className="animate-spin flex justify-center w-5 h-5 border-[3px] border-current border-t-transparent text-orange-600 rounded-full" role="status" aria-label="loading">
             <span className="sr-only">Loading...</span>
         </div>
