8ae65b8aec8ef8f60d0d517adb21f540210c5dbf
Justin Dosaj <jdosaj@vurge.io>
2023-10-26 13:33:04 -0700

    UI & Function Update

--- src/components/shared/tokenAmt.tsx
+++ src/components/shared/tokenAmt.tsx
@@ -1,3 +1,5 @@
+import { VideoCameraIcon } from "@heroicons/react/20/solid"
+
 export interface Props {
     tokens: number,
     className?:string,
@@ -5,15 +7,8 @@ export interface Props {
 
 export function TokenAmount({tokens, className}: Props) {
     return(
-    <div className={`items-center inline-flex gap-1 text-white bg-primary-main px-4 py-3 rounded-full outline-none relative overflow-hidden border duration-300 ease-linear ${className}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-coins" width="" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M9 14c0 1.657 2.686 3 6 3s6 -1.343 6 -3s-2.686 -3 -6 -3s-6 1.343 -6 3z"></path>
-            <path d="M9 14v4c0 1.656 2.686 3 6 3s6 -1.344 6 -3v-4"></path>
-            <path d="M3 6c0 1.072 1.144 2.062 3 2.598s4.144 .536 6 0c1.856 -.536 3 -1.526 3 -2.598c0 -1.072 -1.144 -2.062 -3 -2.598s-4.144 -.536 -6 0c-1.856 .536 -3 1.526 -3 2.598z"></path>
-            <path d="M3 6v10c0 .888 .772 1.45 2 2"></path>
-            <path d="M3 11c0 .888 .772 1.45 2 2"></path>
-        </svg>
+    <div className={`items-center inline-flex gap-1 text-white bg-primary-main px-4 py-3 rounded-full outline-none relative overflow-hidden border duration-300 ease-linear space-x-1 ${className}`}>
+        <VideoCameraIcon className="text-white h-5 w-5"/>
         <p className="font-bold text-lg">{tokens}</p>
     </div>
     )
