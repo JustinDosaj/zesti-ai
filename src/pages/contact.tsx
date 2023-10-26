b48c2fa7ef7b902ed237a487bf781b3a9903831f
Justin Dosaj <jdosaj@vurge.io>
2023-10-26 16:16:41 -0700

    UI and Content Updates
    1. Updated sitemap.xml
    2. Updated hero and link input on dash to mention its free
    3. Updated pricing page ui and content
    4. Fixed bug on mobile devices that causes link input text to be white causing it to not be visible
    5. Updated SEO description tags on every page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -15,6 +15,7 @@ export default function Contact() {
     <>
     <Head>
         <title>Get in Touch | Contact Zesti for Support & Inquiries</title>
+        <meta name="description" content="Have questions or need assistance? Reach out to the Zesti support team through our contact page. We are here to help with all your recipe conversion needs!"/>
     </Head>    
     <main className={`flex min-h-screen flex-col items-center justify-between bg-background  h-screen ${raleway.className}`}>
         <section className="relative my-auto ">
