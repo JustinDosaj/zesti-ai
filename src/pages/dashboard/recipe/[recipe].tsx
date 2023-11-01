4a68aa251734cab2e1a9d8bfff3f23d0d03abb4f
Justin Dosaj <jdosaj@vurge.io>
2023-11-01 11:55:22 -0700

    Massive Update
    Recipe Page Loading Update
    - Dashbaord page now updates recipes in real time without manual refreshing

--- src/pages/dashboard/recipe/[recipe].tsx
+++ src/pages/dashboard/recipe/[recipe].tsx
@@ -8,6 +8,7 @@ import { getRecipe } from "@/pages/api/firebase/functions";
 import { Container } from "@/components/shared/container";
 import { PageLoader } from "@/components/shared/loader";
 import Head from "next/head";
+import { RecipePopOutMenu, EditRecipeInput } from "@/components/recipe/recipe";
 
 const raleway = Raleway({subsets: ['latin']})
 
@@ -16,16 +17,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {props: {id: id}}
 }
 
-function classNames(...classes: (string | undefined | null | false)[]): string {
-return classes.filter(Boolean).join(' ');
-}
 
 const Recipe: React.FC = ({id}: any) => {
 
     const { user, isLoading } = useAuth();
     const [ onFirstLoad, setOnFirstLoad ] = useState<boolean>(true)
-    const [obj, setObj] = useState<any>([])
-    const [url, setUrl] = useState('')
+    const [recipe, setRecipe] = useState<any>([])
+    const [url, setUrl] = useState<string>('')
+    const [edit, setEdit] = useState<boolean>(false)
+    const [editingIngredientIndex, setEditingIngredientIndex] = useState<number | null>(null);
     const router = useRouter();
 
 
@@ -39,18 +39,19 @@ const Recipe: React.FC = ({id}: any) => {
 
     async function onFirstRecipeLoad(){
         const response = await getRecipe(user?.uid, id).then((res) => {
-            setObj(JSON.parse(res?.data.message.content))
+          console.log("RES: ", res)
+            setRecipe(res?.data)
             setUrl(res?.url)
         })
         setOnFirstLoad(false)
     }
 
-    if(!obj.name) return <PageLoader/>
+    if(!recipe.name) return <PageLoader/>
 
     return(
     <>
     <Head>
-      <title>{obj.name}</title>
+      <title>{recipe.name}</title>
     </Head>  
     <main className={`flex min-h-screen flex-col items-center justify-between p-2 bg-background mt-12${raleway.className}`}>
       <Container className={"flex flex-col lg:flex-row gap-10 lg:gap-12 mt-36"}>
@@ -58,23 +59,23 @@ const Recipe: React.FC = ({id}: any) => {
         <div className="md:flex">
           <div className="min-w-0 flex-1 space-y-2">
             <div className="gap-x-3 text-xl font-semibold text-gray-900 grid space-y-2 sm:inline-flex">
-                {obj?.name}
+                {recipe?.name}
                 <div className="mt-2 inline-flex sm:mt-0 sm:space-y-0 gap-x-2 text-center">
-                  { obj.time == null ? 
+                  { recipe.time == null ? 
                   <div></div>
                   :
-                  <p className="bg-green-500 rounded-xl text-white text-sm p-1">{`${obj?.time} Minutes`}</p> 
+                  <p className="bg-green-500 rounded-xl text-white text-sm p-1">{`${recipe?.time} Minutes`}</p> 
                   }
-                  { obj.servings == null ? 
+                  { recipe.servings == null ? 
                   <div></div>
                   :
-                  <p className="bg-green-500 rounded-xl text-white text-sm p-1">{`${obj?.servings} Servings`}</p>
+                  <p className="bg-green-500 rounded-xl text-white text-sm p-1">{`${recipe?.servings} Servings`}</p>
                   }
                 </div>
             </div>
 
             <p className="text-gray-500">
-              {`${obj?.description}`}
+              {`${recipe?.description}`}
             </p>
           </div>
             <button onClick={() => window.open(url)}
@@ -90,18 +91,17 @@ const Recipe: React.FC = ({id}: any) => {
       </Container>
       <Container className={"flex flex-col lg:flex-row gap-10 lg:gap-12 mt-12"}>
         <div className="my-auto overflow-hidden w-full bg-white py-5 border rounded-lg p-4 md:p-12">
-          <h2 className="text-lg font-medium text-gray-500">Ingredients {`(${obj?.ingredients?.length})`}</h2>
+          <h2 className="text-lg font-medium text-gray-500">Ingredients {`(${recipe?.ingredients?.length})`}</h2>
           <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
-            {obj?.ingredients?.map((ingred: any, index: any) => (
+            {recipe?.ingredients?.map((ingred: any, index: any) => (
               <li key={index} className="col-span-1 flex rounded-md shadow-sm">
-                <div className="flex rounded-md overflow-hidden w-full">
+                <div className="flex rounded-md overflow-visible w-full">
                   <div className="bg-amber-400 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md font-md text-white" >
                     {index + 1}
                   </div>
                   <div className="flex flex-1 items-center justify-between rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-                    <div className="flex-1 truncate px-4 py-2 text-sm">
-                      <p className="text-gray-500 overflow-ellipsis whitespace-normal">{ingred}</p>
-                    </div>
+                    <EditRecipeInput edit={edit} setEdit={setEdit} ingredient={ingred} isEditing={editingIngredientIndex === index} setEditingIngredientIndex={setEditingIngredientIndex} index={index}/>
+                    <RecipePopOutMenu edit={edit} setEdit={setEdit} setEditingIngredientIndex={setEditingIngredientIndex} index={index}/>
                   </div>
                 </div>
               </li>
@@ -111,11 +111,11 @@ const Recipe: React.FC = ({id}: any) => {
       </Container>
       <Container className={"flex flex-col lg:flex-row gap-10 lg:gap-12 mt-12 mb-36"}>
         <div className="my-auto overflow-hidden bg-white py-5 border w-full rounded-lg p-4 md:p-12 ">
-          <h2 className="text-lg font-medium text-gray-500">Instructions {`(${obj?.instructions?.length})`}</h2>
+          <h2 className="text-lg font-medium text-gray-500">Instructions {`(${recipe?.instructions?.length})`}</h2>
           <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
-            {obj?.instructions?.map((instruct: any, index: any) => (
+            {recipe?.instructions?.map((instruct: any, index: any) => (
               <li key={index} className="col-span-1 flex rounded-md shadow-sm">
-                <div className="flex rounded-md overflow-hidden w-full">
+                <div className="flex rounded-md overflow-visible w-full">
                   <div className="bg-green-600 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md font-medium text-white" >
                     {index + 1}
                   </div>
@@ -123,6 +123,7 @@ const Recipe: React.FC = ({id}: any) => {
                     <div className="flex-1 truncate px-4 py-2 text-sm">
                       <p className="text-gray-500 overflow-ellipsis whitespace-normal">{instruct}</p>
                     </div>
+                    <RecipePopOutMenu edit={edit} setEdit={setEdit} setEditingIngredientIndex={setEditingIngredientIndex} index={index}/>
                   </div>
                 </div>
               </li>
