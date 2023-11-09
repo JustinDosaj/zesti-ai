54caadf3fb1d72c23447670a8a5e0ef9a1c7f4ec
Justin Dosaj <jdosaj@vurge.io>
2023-11-09 12:37:10 -0800

    Scroll Update

--- src/components/chat/chatbox.tsx
+++ src/components/chat/chatbox.tsx
@@ -1,8 +1,9 @@
-import { useState, ChangeEvent, MouseEvent, useEffect } from 'react';
+import React, { useRef, useState, ChangeEvent, MouseEvent, useEffect } from 'react';
 import { ChatBubbleLeftEllipsisIcon, PaperAirplaneIcon } from '@heroicons/react/24/outline';
 import { db } from '@/pages/api/firebase/firebase';
 import { useAuth } from '@/pages/api/auth/auth';
 import { doc, updateDoc, arrayUnion, serverTimestamp, setDoc, collection, onSnapshot, query, orderBy, where, Timestamp } from 'firebase/firestore';
+import { InlineBtnLink } from '../shared/inline-btn-link';
 
 interface Message {
   id: string;
@@ -17,6 +18,11 @@ export function Chatbox() {
   const [message, setMessage] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
   const { user } = useAuth();
+  const endOfMessagesRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   useEffect(() => {
     if (user) {
@@ -35,6 +41,7 @@ export function Chatbox() {
           ...doc.data() as Message
         }));
         setMessages(fetchedMessages);
+        scrollToBottom();
       });
 
       return () => unsubscribe();
@@ -73,27 +80,44 @@ export function Chatbox() {
       }, { merge: true });
   
       setMessage(''); // Clear the message input after sending
+      scrollToBottom();
 
     } catch (error) {
       console.error('Error sending message:', error);
     }
   };
 
+  useEffect(() => {
+    scrollToBottom(); // Scroll to bottom every time messages change
+  }, [messages]);
+
   return (
-    <div className="fixed bottom-4 right-4">
+    <div className="fixed bottom-4 right-4 z-[10]">
       {isOpen ? (
-        <div className="w-64 md:w-96 h-96 bg-white rounded-lg shadow-lg flex flex-col" onClick={handleChatboxClick}>
-          <div className="p-4 flex justify-between items-center border-b">
-            <h2 className="text-lg font-semibold">Talk to Zesti</h2>
+        <div className="w-full h-96 p-4 md:w-[500px] md:h-[500px] bg-white rounded-lg shadow-lg flex flex-col border border-gray-200" onClick={handleChatboxClick}>
+          <div className="p-4 flex justify-between items-center border-b border-gray-200">
+            <h2 className="text-lg font-semibold">Zesti Cooking Assistant</h2>
             <button onClick={() => setIsOpen(false)} className="text-xl">×</button>
           </div>
+          {user ? 
           <div className="flex-1 p-4 overflow-y-auto ">
           {messages.map(({ id, sender, text }) => (
               <div key={id} className={`border p-2 rounded-xl message ${sender === 'user' ? 'user-message bg-primary-main bg-opacity-90 justify-items-end w-fit text-white mb-3' : 'bg-gray-100 bot-message mb-3'}`}>
                 {text}
               </div>
             ))}
+            <div ref={endOfMessagesRef} />
           </div>
+          :
+          <div className="flex-1 p-4 overflow-y-auto ">
+              <div className={`border p-2 rounded-xl message bg-primary-main bg-opacity-90 justify-items-end w-fit text-black mb-3`}>
+                <span className="text-white">Please login to use chat</span>
+              </div>
+              <div className={`border p-2 rounded-xl message bg-primary-main bg-opacity-90 justify-items-end w-fit text-black mb-3`}>
+                <InlineBtnLink href="/login" text="Click Here to Login" className="text-white mr-1"/>
+              </div>
+          </div>
+          }
           <div className="flex items-center m-4 focus:outline-none focus:ring border rounded-lg">
             <input
               type="text"
@@ -105,7 +129,7 @@ export function Chatbox() {
             <button
               type="submit"
               onClick={handleSendMessage}
-              className="bg-primary-main text-white rounded-r p-2 hover:bg-primary-alt transition"
+              className={`bg-primary-main text-white rounded-r p-2 hover:bg-primary-alt transition ${!user ? `hover:cursor-not-allowed` : ``}`}
             >
               <PaperAirplaneIcon className="w-6 h-6" />
             </button>
