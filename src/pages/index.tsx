e3a132977c853cb8506026c0404f6087287aabd4
Justin Dosaj <jdosaj@vurge.io>
2023-11-17 12:47:47 -0800

    Remove hub from pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import { Optimize } from '@/components/home-sections/optimize'
 import Head from 'next/head';
 import { Reach } from '@/components/home-sections/reach';
 import HowItWorks from '@/components/home-sections/howitworks';
-import Hub from '@/components/hub/hub';
 
 const raleway = Raleway({subsets: ['latin']})
 
