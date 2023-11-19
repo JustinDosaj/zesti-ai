96c08ac42c32d8d03cd95e9079838925cb734e2c
Justin Dosaj <jdosaj@vurge.io>
2023-11-19 10:31:05 -0800

    Fixed Website Tool Display

--- src/pages/tools/website.tsx
+++ src/pages/tools/website.tsx
@@ -1,11 +1,10 @@
 import { Raleway } from 'next/font/google'
-import { Hero } from '@/components/home-sections/hero'
 import Head from 'next/head';
 import { UrlComponent, UrlHero, UrlTips } from '@/components/hub/url';
 import { useAuth } from '../api/auth/auth';
 import { PricingDisplay } from '@/components/pricing-sections/pricing';
 import { FAQ } from '@/components/home-sections/faq';
-
+import { VideoTips } from '@/components/hub/video';
 const raleway = Raleway({subsets: ['latin']})
 
 export default function Website() {
@@ -19,17 +18,17 @@ export default function Website() {
         <meta name="title" content="Zesti AI | Ad-Free Web Recipe"/>
         <meta name="description" content="Tired of Ads? Avoid searching through endless clutter and simply input the web URL into Zesti for a recipe you want to enjoy!"/>
       </Head>
-      <main className={`flex min-h-screen flex-col items-center bg-background ${raleway.className}`}>
-      <UrlHero/>
-      { stripeRole == 'premium' ?
-        <div>
-          <UrlComponent/>
-          <UrlTips/>
-        </div>
-        :
-        <PricingDisplay/>
-      }
-      <FAQ/>
+      <main className={` flex min-h-screen flex-col items-center bg-background ${raleway.className}`}>
+        <UrlHero/>
+        { stripeRole == 'premium' ?
+          <div>
+            <UrlComponent/>
+            <UrlTips/>
+          </div>
+          :
+          <PricingDisplay/>
+        }
+        <FAQ/>
       </main>
     </>
   )
