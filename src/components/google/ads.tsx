0119e5f399e379619361bc0ee08c6805bb3c7595
Justin Dosaj <jdosaj@vurge.io>
2023-11-20 11:50:57 -0800

    Google Adsense Snippet

---
+++ src/components/google/ads.tsx
@@ -0,0 +1,33 @@
+import { Container } from "../shared/container"
+
+export function RecipePageAmazonProduct(){
+
+    return(
+        <Container className={"flex flex-col lg:flex-row gap-10 lg:gap-12 mt-12 mb-36"}>
+        <div className="my-auto overflow-hidden bg-white py-5 border w-full rounded-lg p-4 md:p-12 ">
+          <h2 className="text-sm font-medium text-gray-500 bg-gray-200 w-fit p-2 rounded-xl">Ad</h2>
+          <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+            {/*recipe?.instructions?.map((instruct: any, index: any) => (
+              <li key={index} className="col-span-1 flex rounded-md shadow-sm">
+                <div className="flex rounded-md overflow-visible w-full">
+                  <div className="bg-green-600 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md font-medium text-white" >
+                    {index + 1}
+                  </div>
+                  <div className="flex flex-1 items-center justify-between rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+
+                  </div>
+                </div>
+              </li>
+            ))*/}
+          </ul>
+        </div>
+      </Container>
+    )
+}
+
+export function RecipePageGoogleAd() {
+    return(
+    <>
+    </>
+    )
+}
