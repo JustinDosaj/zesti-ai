3e8a0727b14e32e9dcaf48da00de840003597299
Justin Dosaj <jdosaj@vurge.io>
2023-11-20 13:13:02 -0800

    Tool Page Google Ad

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
 import { Raleway } from 'next/font/google'
-import { Hero, Hero2 } from '@/components/home-sections/hero'
+import { Hero2 } from '@/components/home-sections/hero'
 import { FAQ } from '@/components/home-sections/faq';
 import { Optimize } from '@/components/home-sections/optimize'
 import Head from 'next/head';
 import { Reach } from '@/components/home-sections/reach';
 import HowItWorks from '@/components/home-sections/howitworks';
+import GoogleTags from '@/components/google/conversion';
 
 const raleway = Raleway({subsets: ['latin']})
 
 export default function Home() {
   // Comment
+
   return (
     <>
       <Head>
         <title>Zesti AI | AI Cooking Assistant | Chat, Save & Edit</title>
         <meta name="title" content="Zesti AI | AI Cooking Assistant | Chat, Save & Edit"/>
         <meta name="description" content="Easily save & edit recipes found from cooking videos and chat with our AI tool powered by OpenAI and ChatGPT. Try for free. No credit card required."/>
+        <GoogleTags/>
       </Head>
       <main className={`flex min-h-screen flex-col items-center justify-between bg-background ${raleway.className}`}>
         <Hero2/>
