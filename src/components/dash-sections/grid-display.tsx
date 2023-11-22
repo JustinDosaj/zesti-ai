3136707f43c3f2f16e963deeb63b86887d41fa31
Justin Dosaj <jdosaj@vurge.io>
2023-11-21 16:57:27 -0800

    Temporary TIktok fix/Mobile Fix

--- src/components/dash-sections/grid-display.tsx
+++ src/components/dash-sections/grid-display.tsx
@@ -35,9 +35,9 @@ export function GridDisplay({data}: GridDisplayProps) {
     //const { user } = useAuth
 
     return(
-    <Container className={"flex flex-col lg:flex-wrap gap-10 lg:gap-4 mb-28"}>
+    <Container className={"flex flex-col lg:flex-wrap gap-10 lg:gap-4 mb-28 "}>
       <h3 className="text-2xl font-bold leading-6 text-gray-900 text-center">Saved Recipes</h3>
-      <ul role="list" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8">
+      <ul role="list" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8 max-h-screen overflow-y-scroll md:overflow-auto md:max-h-max">
       {data.map((recipe: any) => {
         if (recipe.complete == false) {
           return(
