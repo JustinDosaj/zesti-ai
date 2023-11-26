9cf8b62abe64ee186079b07180eb5c35cef257c9
Justin Dosaj <jdosaj@vurge.io>
2023-11-25 17:21:47 -0800

    Remove userinput on cancel

--- src/components/shared/modals.tsx
+++ src/components/shared/modals.tsx
@@ -317,7 +317,7 @@ export function AddToRecipeModal({isOpen, setIsOpen, addType, onSubmit}: AddProp
                   <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                     <SquaresPlusIcon className="h-6 w-6 text-color-alt-green" aria-hidden="true" />
                   </div>
-                  <div className="mt-3 text-center sm:mt-5">
+                  <div className="mt-3 text-center sm:mt-5 ">
                     <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900 capitalize">
                       {`Add ${addType}`}
                     </Dialog.Title>
@@ -338,7 +338,10 @@ export function AddToRecipeModal({isOpen, setIsOpen, addType, onSubmit}: AddProp
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                    onClick={() => setIsOpen(false)}
+                    onClick={() => {
+                      setUserInput('')
+                      setIsOpen(false)
+                    }}
                     ref={cancelButtonRef}
                   >
                     Cancel
