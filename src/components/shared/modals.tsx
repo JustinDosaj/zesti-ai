6f7ae629ed2844ab196d217719597f5800956b67
Justin Dosaj <jdosaj@vurge.io>
2023-12-11 13:25:32 -0800

    Update

--- src/components/shared/modals.tsx
+++ src/components/shared/modals.tsx
@@ -86,7 +86,7 @@ export function InputResponseModal({isOpen, setIsOpen, success, message, role}: 
                 </div>
                 }
                 { role !== 'premium' ?
-                <div className="py-4">
+                <div className="py-4 overflow-hidden w-fit object-contain">
                   <AdSense adSlot="9250004753"/>
                 </div>
                 :
