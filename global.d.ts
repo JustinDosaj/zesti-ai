1d000cdf06653123989aca988969727bf6dd488a
Justin Dosaj <jdosaj@vurge.io>
2023-12-15 14:02:34 -0800

    Referral Test

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,17 @@
 declare global {
     interface Window {
       adsbygoogle: any[];
+      Rewardful?:{
+        referral?:string;
+      }
+    }
+
+    interface CheckoutSessionData {
+      price: string;
+      success_url: string;
+      cancel_url: string;
+      mode: string;
+      clientReferenceId?: string;
     }
   }
   
--- src/pages/api/stripe/stripePremium.ts
+++ src/pages/api/stripe/stripePremium.ts
@@ -1,16 +1,33 @@
 import { collection, doc, addDoc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
 
+
+interface CheckoutSessionData {
+    price: string | undefined;
+    success_url: string;
+    cancel_url: string;
+    mode: string;
+    clientReferenceId?: string; // Make sure this line is included
+}
+
 export async function createPremiumCheckoutSession(id: any) {  // You might want to replace 'any' with the appropriate type for currentUser
 
     try {
-        const checkoutSessionsCollection = collection(doc(collection(db, 'users'), id), 'checkout_sessions');
-        const docRef = await addDoc(checkoutSessionsCollection, {
+        const checkoutSessionData: CheckoutSessionData  = {
             price: process.env.NEXT_PUBLIC_STRIPE_PREMIUM_PRICE,
             success_url: `${window.location.origin}/dashboard`,
             cancel_url: `${window.location.origin}/dashboard`,
             mode: "subscription",
-        });
+        };
+
+        if (window.Rewardful?.referral) {
+            checkoutSessionData.clientReferenceId = window.Rewardful.referral;
+        }
+
+
+        const checkoutSessionsCollection = collection(doc(collection(db, 'users'), id), 'checkout_sessions');
+        const docRef = await addDoc(checkoutSessionsCollection, checkoutSessionData);
+
 
         onSnapshot(docRef, (snap) => {
             const data = snap.data();
