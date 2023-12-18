dec32677158b922e49d19a2b3379a1ba185dce3e
Justin Dosaj <jdosaj@vurge.io>
2023-12-18 13:51:58 -0800

    Fix Ad Display

--- src/components/dash-sections/recipelist.tsx
+++ src/components/dash-sections/recipelist.tsx
@@ -44,7 +44,7 @@ export function RecipeList({data, maxDisplayCount = -1}:StackListProps & {maxDis
   const [ selectedRecipeId, setSelectedRecipeId ] = useState<string | null>(null);
 
   return(
-    <Container className={"flex flex-col lg:flex-wrap gap-10 lg:gap-4 pb-28 mt-4 animate-fadeIn"}>
+    <Container className={"flex flex-col lg:flex-wrap gap-10 lg:gap-4 mt-4 animate-fadeIn"}>
       <ul role="list" className="divide-y divide-primary-main divide-opacity-50 sm:divide-gray-300 text-gray-700">
         {sortedData.slice(0, maxDisplayCount >= 0 ? maxDisplayCount : data.length).map((recipe) => (
             <li key={recipe.id} className="flex items-center justify-between gap-x-6 py-5">
