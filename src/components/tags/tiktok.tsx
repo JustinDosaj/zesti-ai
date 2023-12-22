7da535dbbefd8b77a9707e08f5ecbef56991b819
Justin Dosaj <jdosaj@vurge.io>
2023-12-22 13:57:19 -0800

    Conversion Tag Update

--- src/components/tags/tiktok.tsx
+++ src/components/tags/tiktok.tsx
@@ -1,41 +1,42 @@
 
 export default function TikTikConversionTags() {
     return(
-    <>
-        <script dangerouslySetInnerHTML={{
-            __html: `
-            !function(w, d, t) {
-                w.TiktokAnalyticsObject = t;
-                var ttq = w[t] = w[t] || [];
-                ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"];
-                ttq.setAndDefer = function(t, e) {
-                  t[e] = function() {
-                    t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+      <>
+      <script dangerouslySetInnerHTML={{
+          __html: `
+          !function (w, d, t) {
+              w.TiktokAnalyticsObject = t;
+              var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"];
+              ttq.setAndDefer = function (t, e) {
+                  t[e] = function () {
+                      t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
                   }
-                };
-                for (var i = 0; i < ttq.methods.length; i++) {
+              };
+              for (var i = 0; i < ttq.methods.length; i++) {
                   ttq.setAndDefer(ttq, ttq.methods[i]);
-                }
-                ttq.instance = function(t) {
+              }
+              ttq.instance = function (t) {
                   for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) {
-                    ttq.setAndDefer(e, ttq.methods[n]);
+                      ttq.setAndDefer(e, ttq.methods[n]);
                   }
                   return e;
-                };
-                ttq.load = function(e, n) {
+              };
+              ttq.load = function (e, n) {
                   var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
                   ttq._i = ttq._i || {}, ttq._i[e] = [], ttq._i[e]._u = i, ttq._t = ttq._t || {}, ttq._t[e] = +new Date, ttq._o = ttq._o || {}, ttq._o[e] = n || {};
                   var o = d.createElement("script");
                   o.type = "text/javascript", o.async = true, o.src = i + "?sdkid=" + e + "&lib=" + t;
                   var a = d.getElementsByTagName("script")[0];
                   a.parentNode.insertBefore(o, a);
-                };
-        
-                ttq.load('${process.env.NEXT_PUBLIC_TIKTOK_CONVERSION_ID}');
-                ttq.page();
-              }(window, document, 'ttq');
-            `
-        }} />
+              };
+
+              ttq.load('${process.env.NEXT_PUBLIC_TIKTOK_CONVERSION_ID}');
+              ttq.page();
+          }(window, document, 'ttq');
+          `
+      }} />
     </>
     )
 }
+
