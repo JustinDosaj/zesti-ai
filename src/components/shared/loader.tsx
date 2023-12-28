bc02f72f7d44e1856cbd4eda5dad3c30fc25daf6
Justin Dosaj <jdosaj@vurge.io>
2023-12-27 20:22:34 -0800

    Bring Back Tool Loader

--- src/components/shared/loader.tsx
+++ src/components/shared/loader.tsx
@@ -50,3 +50,20 @@ export function PageLoader() {
     </>
     )
 }
+
+export function ToolLoader() {
+    return(
+    <>
+    <Head>
+      <title>Zesti | Loading...</title>
+    </Head>  
+    <main className={`flex min-h-screen flex-col items-center justify-between p-2 bg-background ${raleway.className}`}>
+        <div className="grid justify-center bg-white px-6 py-3 outline-none relative overflow-hidden duration-300 ease-linear">
+            <div className="animate-spin flex justify-center w-10 h-10 border-[3px] border-current border-t-transparent text-orange-600 rounded-full" role="status" aria-label="loading">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    </main>
+    </>
+    )
+}
