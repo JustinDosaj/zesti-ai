a0b4544064de46c1a859bfb93fe83387a2c3d9eb
Justin Dosaj <jdosaj@vurge.io>
2023-12-28 12:52:54 -0800

    Checking PromoteKit

--- src/components/tags/headertags.tsx
+++ src/components/tags/headertags.tsx
@@ -8,3 +8,9 @@ export function RewardfulTag() {
         </>
     )
 }
+
+export function PromoteKitTag(){
+    return(
+        <script async src="https://cdn.promotekit.com/promotekit.js" data-promotekit="8fee0c20-1351-4c3c-b2dc-d51f88aee32f"></script>
+    )
+}
