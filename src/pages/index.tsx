eef0238bc316c278024e5ed5cb44c0baba76a6c0
Justin Dosaj <jdosaj@vurge.io>
2023-12-28 12:52:54 -0800

    Checking PromoteKit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import GoogleTags from '@/components/tags/conversion';
 import { HomePageScroller, HomePageTools, HomePageCTA, HomeFAQ, HomeVideoToRecipe, Hero } from '@/components/home';
 import { useAuth } from './api/auth/auth';
 import { PageLoader } from '@/components/shared/loader';
-import { RewardfulTag } from '@/components/tags/headertags';
+import { PromoteKitTag } from '@/components/tags/headertags';
 
 const raleway = Raleway({subsets: ['latin']})
 
@@ -21,7 +21,7 @@ export default function Home() {
         <meta name="title" content="Zesti | Save Tiktok & Youtube Recipes | AI Recipe Generator"/>
         <meta name="description" content="Easily save & edit recipes found from cooking youtube & tiktok videos. Plus chat with our AI tool powered by OpenAI and ChatGPT. Try for free. No credit card required."/>
         <GoogleTags/>
-        <RewardfulTag/>
+        <PromoteKitTag/>
       </Head>
       <main className={`flex min-h-screen flex-col items-center justify-between bg-background ${raleway.className}`}>
         <Hero titleStart={"Save, Create & Customize"} titleEnd={"Recipes"} description={"Create AI generated recipes or instantly save your favorite recipes from YouTube & Tiktok!"} loginURL={"/login"}/>
