56a4ff11562b9ab68169f0613b4bd9d8cd141d45
Justin Dosaj <jdosaj@vurge.io>
2024-01-03 15:20:37 -0800

    Update Recipes Structure in Firestore
    1. Removed data array from recipe structure in firestore

--- src/components/dashboard/recipelist.tsx
+++ src/components/dashboard/recipelist.tsx
@@ -14,18 +14,16 @@ interface Recipe {
   id: string;
   date: any;
   status: string,
-  data: {
-    name: string,
-    ingredients: [],
-    instructions: [],
-    servings: string,
-    time: string
-  }
+  name: string,
+  ingredients: [],
+  instructions: [],
+  servings: string,
+  time: string,
   // other fields
 }
 
 interface StackListProps {
-  data: Recipe[];
+  data?: Recipe[];
   title?: any,
 }
 
@@ -41,7 +39,7 @@ export function RecipeList({data, title, maxDisplayCount = 10, incrementCount = 
   const router = useRouter()
 
   // Sort data to display most recent recipes first
-  const sortedData = data.sort((a, b) => {
+  const sortedData = data?.sort((a, b) => {
     const dateA = new Date(a.date).getTime();
     const dateB = new Date(b.date).getTime();
     return dateB - dateA;
@@ -55,7 +53,7 @@ export function RecipeList({data, title, maxDisplayCount = 10, incrementCount = 
     <Container className={"flex flex-col lg:flex-wrap gap-10 lg:gap-4 mt-4 animate-fadeIn"}>
       <SharedListTitle title={title}/>
       <ul role="list" className="divide-y divide-primary-main divide-opacity-50 sm:divide-gray-300 text-gray-700">
-        {sortedData.slice(0, displayCount).map((recipe) => (
+        {sortedData?.slice(0, displayCount).map((recipe) => (
             <li key={recipe.id} className="flex items-center justify-between gap-x-6 py-5">
               <div className="min-w-0">
                 <div className="flex items-start gap-x-3">
@@ -63,7 +61,7 @@ export function RecipeList({data, title, maxDisplayCount = 10, incrementCount = 
                     href={{pathname: `/dashboard/recipe/[recipeId]`,
                     query: {recipeId: recipe.id}
                     }}
-                    className="text-base sm:text-lg font-semibold leading-6 text-gray-700 hover:text-gray-500">{recipe.status == "Complete" ? recipe.data.name : recipe.status == "Failed" ? "Error getting recipe. Please try again later" : "Loading, please wait..."}</Link>
+                    className="text-base sm:text-lg font-semibold leading-6 text-gray-700 hover:text-gray-500">{recipe.status == "Complete" ? recipe.name : recipe.status == "Failed" ? "Error getting recipe. Please try again later" : "Loading, please wait..."}</Link>
                     <p
                     className={
                       recipe.status == "Complete" ? 
@@ -80,20 +78,20 @@ export function RecipeList({data, title, maxDisplayCount = 10, incrementCount = 
                     </p>
                 </div>
                 <div className="mt-1 flex items-center gap-x-2 text-xs sm:text-sm leading-5 text-gray-500">
-                  <p className={recipe.data?.time ? `whitespace-nowrap` : `hidden`}>
-                    <span>{recipe.data ? `${recipe.data?.time} Minutes` : ''}</span>
+                  <p className={recipe?.time ? `whitespace-nowrap` : `hidden`}>
+                    <span>{recipe ? `${recipe?.time} Minutes` : ''}</span>
                   </p>
-                  <svg viewBox="0 0 2 2" className={recipe.data?.time ? `h-0.5 w-0.5 fill-current` : `hidden`}>
+                  <svg viewBox="0 0 2 2" className={recipe?.time ? `h-0.5 w-0.5 fill-current` : `hidden`}>
                     <circle cx={1} cy={1} r={1} />
                   </svg>
                   <p className="whitespace-nowrap">
-                    <span>{recipe.data ? recipe.data.ingredients.length : '0'} Ingredients</span>
+                    <span>{recipe ? recipe.ingredients.length : '0'} Ingredients</span>
                   </p>
                   <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
                     <circle cx={1} cy={1} r={1} />
                   </svg>
                   <p className="whitespace-nowrap">
-                    <span>{recipe.data ? recipe.data.instructions.length : '0'} Steps</span>
+                    <span>{recipe ? recipe.instructions.length : '0'} Steps</span>
                   </p>
                   <svg viewBox="0 0 2 2" className="hidden sm:block h-0.5 w-0.5 fill-current">
                     <circle cx={1} cy={1} r={1} />
@@ -174,13 +172,13 @@ export function RecipeList({data, title, maxDisplayCount = 10, incrementCount = 
               </div>
             </li>
           ))}
-            {displayCount < data.length && maxDisplayCount >= 10 && (
+            {/*displayCount < data?.length && maxDisplayCount >= 10 && (
                 <div className="flex justify-center py-6">
                     <button onClick={handleLoadMore} className="bg-primary-main rounded-3xl hover:bg-primary-alt text-white font-semibold py-2 px-4">
                         Load More
                     </button>
                 </div>
-            )}
+            )*/}
           <DeleteConfirmationModal isOpen={isDeleteOpen} setIsOpen={setIsDeleteOpen} recipeId={selectedRecipeId}/>
         </ul>
     </Container>
