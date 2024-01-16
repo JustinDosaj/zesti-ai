0c59c6e053173d757f9581c98a2d68c892c15ef7
Justin Dosaj <jdosaj@vurge.io>
2024-01-16 13:20:49 -0800

    Removed 2 Title Shares

--- src/components/shared/title.tsx
+++ src/components/shared/title.tsx
@@ -24,35 +24,6 @@ interface SharedPageTitleProps {
     className?: any
 }
 
-export function SharedPageTitle({title, desc}: SharedPageTitleProps) {
-    return(
-        <section className="pt-32">
-            <Container className={"flex flex-col lg:flex-row gap-10 lg:gap-12 animate-fadeIn"}>
-                <span className="w-4/12 lg:w-2/12 aspect-square bg-gradient-to-tr to-primary from-primaryteal absolute -top-5 lg:left-0 rounded-full skew-y-12 blur-2xl opacity-40 skew-x-12 rotate-90"></span>
-                <div className="relative flex flex-col items-center text-center lg:py-7 xl:py-8 lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2">
-                    <h1 className="text-3xl/tight sm:text-4xl/tight md:text-5xl/tight xl:text-5xl/tight
-                    font-bold text-heading-1 text-black">
-                    {title}
-                    </h1>
-                    <Paragraph className="mt-4 text-gray-600">
-                        {desc}
-                    </Paragraph>
-                </div>
-            </Container>
-        </section>
-    )
-}
-
-export function SharedListTitle({title, desc}: SharedPageTitleProps) {
-    return(
-    <Container className="relative sm:flex justify-center items-center lg:flex-wrap gap-10 lg:gap-4 w-full animate-fadeIn">
-        <h3 className="text-2xl font-bold leading-6 text-gray-900 text-center">{title}</h3>
-        <Paragraph className="mt-4 text-gray-600 text-center">
-            {desc}
-        </Paragraph>
-    </Container>
-    )
-}
 
 export function SharedViewAllTitle({title, href, desc}: SharedPageTitleProps) {
     return(
