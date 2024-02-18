03c6babef71c44c9c98f2ae01218f5e6f3fd2e4e
Justin Dosaj <jdosaj@vurge.io>
2024-02-18 10:27:07 -0800

    UI Fixes
    1. TikTok link on creator page now navigates correctly
    
    2. Fixed breadcrumb spacing on recipe pages  when home + creator name + recipe id is too long

--- src/components/shared/breadcrumb.tsx
+++ src/components/shared/breadcrumb.tsx
@@ -1,8 +1,10 @@
 // Breadcrumbs component
 import { useNavigation } from '@/pages/api/context/navigation';
 import Link from 'next/link';
+import useCreatorRecipe from '@/hooks/creator/useCreatorRecipe';
 
 const Breadcrumbs = () => {
+  
   const { breadcrumb } = useNavigation();
 
   return (
@@ -10,7 +12,7 @@ const Breadcrumbs = () => {
       <ol className="list-none p-0 inline-flex items-center pb-4">
         {breadcrumb.map((name, index) => (
           <li key={index} className="inline">
-            {index > 0 && <span className="text-gray-700 mx-2">/</span>}
+            {index > 0 && <span className="text-gray-700 mx-1 text-xs lg:mx-3 lg:text-base">/</span>}
             {index < breadcrumb.length - 1 ? (
               <Link href={
                 name === 'Home' ? '/' 
@@ -20,10 +22,12 @@ const Breadcrumbs = () => {
                 name === 'Add Recipe' ? '/creator/edit/add-recipe'
                 :
                 `/${name.toLowerCase().replace(/\s+/g, '-')}`}>
+
                 <span className="text-primary-main hover:text-primary-alt lowercase">{name}</span>
+
               </Link>
             ) : (
-              <span className="text-gray-700 lowercase">{name}</span>
+              <span className="text-gray-700 lowercase text-sm lg:text-base">{name}</span>
             )}
           </li>
         ))}
