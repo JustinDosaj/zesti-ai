c55ece4160d8694a701e37fa5138814855af7608
Justin Dosaj <jdosaj@vurge.io>
2024-03-06 13:40:21 -0800

    UI Fixes

--- src/components/profile/user.tsx
+++ src/components/profile/user.tsx
@@ -14,7 +14,7 @@ export function ProfilePageComponent() {
     return (
         <Container className={"mt-8 flex flex-col lg:flex-row gap-10 lg:gap-12 pb-0 xl:pb-24"}>
             <div className="mx-auto max-w-2xl lg:flex lg:gap-x-16 lg:px-8 py-8 standard-component-border w-full">
-                <main className="px-4 sm:px-6 lg:flex-auto lg:px-0 ">
+                <main className="px-4 sm:px-6 lg:flex-auto lg:px-0 w-[325px] lg:w-[500px]">
                     <div className="mx-auto max-w-2xl space-y-10 lg:mx-0 lg:max-w-none">
                         <div>
                             <CreatorTitleComponent title={"Basic Profile Information"} desc={"Manage account and access basic information"}/>
@@ -25,7 +25,7 @@ export function ProfilePageComponent() {
                                     desc={user?.email} 
                                     buttonName="Logout"
                                 />
-                                {stripeRole == 'premium' ? 
+                                {stripeRole !== 'premium' ? // If premium -> manage account; if free -> CTA
                                 <SimpleProfileComponent
                                     onButtonClick={() => {window.open(`${process.env.NEXT_PUBLIC_STRIPE_NO_CODE_PORATL}`)}} 
                                     title={"Subscription"} 
@@ -37,41 +37,42 @@ export function ProfilePageComponent() {
                                     onButtonClick={() => router.push('/about/pricing')}
                                     title={"Subscription"} 
                                     buttonName={"Start Free Trial"} 
-                                    desc={stripeRole}
                                 />
                                 }
                                 <SimpleProfileComponent title={"Monthly Usage Remaining"} desc={userData?.tokens} buttonName={''}/>
+                                {user && userData?.tiktokAccessToken && accountStatus !== 'creator' ? 
+                                    <div className={accountStatus == 'creator' ? 'hidden' : 'block'}>
+                                        <dl className="space-y-6 text-sm leading-6">
+                                            <div className="pt-6 flex justify-between items-center">
+                                                <dt className="font-semibold text-gray-900 sm:w-64 sm:flex-none sm:pr-6 text-sm lg:text-base">Join Creator Program</dt>
+                                                <dd className=" flex gap-x-6 sm:mt-0">
+                                                <button type="button" className="font-semibold text-primary-main hover:text-primary-alt text-sm lg:text-base"
+                                                        onClick={() => router.push('https://zesti.promotekit.com')}>
+                                                        Apply
+                                                    </button>
+                                                </dd>
+                                            </div>
+                                        </dl>
+                                    </div>
+                                    :
+                                    <div className={accountStatus == 'creator' ? 'hidden' : 'block'}>
+                                        <dl className="space-y-6 text-sm leading-6">
+                                            <div className="pt-6 flex justify-between items-center">
+                                                <dt className="font-semibold text-gray-900 sm:w-64 sm:flex-none sm:pr-6 text-sm lg:text-base">Join Creator Program</dt>
+                                                <dd className=" flex gap-x-6 sm:mt-0">
+                                                <button type="button" className="font-semibold text-primary-main hover:text-primary-alt text-sm lg:text-base"
+                                                        onClick={loginWithTikTok}>
+                                                        Connect TikTok
+                                                    </button>
+                                                </dd>
+                                            </div>
+                                        </dl>
+                                    </div>
+                                }
                             </dl>
+                            
                         </div>
-                        {user && userData?.tiktokAccessToken && accountStatus !== 'creator' ? 
-                        <div className={accountStatus == 'creator' ? 'hidden' : 'block'}>
-                            <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
-                                <div className="pt-6 flex justify-between items-center">
-                                    <dt className="font-semibold text-gray-900 sm:w-64 sm:flex-none sm:pr-6 text-sm lg:text-base">Join Creator Program</dt>
-                                    <dd className=" flex gap-x-6 sm:mt-0">
-                                    <button type="button" className="font-semibold text-primary-main hover:text-primary-alt text-sm lg:text-base"
-                                            onClick={() => router.push('https://zesti.promotekit.com')}>
-                                            Apply
-                                        </button>
-                                    </dd>
-                                </div>
-                            </dl>
-                        </div>
-                        :
-                        <div className={accountStatus == 'creator' ? 'hidden' : 'block'}>
-                            <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
-                                <div className="pt-6 flex justify-between items-center">
-                                    <dt className="font-semibold text-gray-900 sm:w-64 sm:flex-none sm:pr-6 text-sm lg:text-base">Join Creator Program</dt>
-                                    <dd className=" flex gap-x-6 sm:mt-0">
-                                    <button type="button" className="font-semibold text-primary-main hover:text-primary-alt text-sm lg:text-base"
-                                            onClick={loginWithTikTok}>
-                                            Connect TikTok
-                                        </button>
-                                    </dd>
-                                </div>
-                            </dl>
-                        </div>
-                         }
+                        
                     </div>
                 </main>
             </div>
