53b746f5461d1074218d8b9a142579001c855037
Justin Dosaj <jdosaj@vurge.io>
2024-03-11 16:06:25 -0700

    Testing Update

--- src/components/ui/user/account.tsx
+++ src/components/ui/user/account.tsx
@@ -14,7 +14,7 @@ export function ProfilePageComponent() {
     return (
         <Container className={"mt-8 flex flex-col lg:flex-row gap-10 lg:gap-12 pb-0 xl:pb-24"}>
             <div className="mx-auto max-w-2xl lg:flex lg:gap-x-16 lg:px-8 py-8 standard-component-border w-full">
-                <main className="px-4 sm:px-6 lg:flex-auto lg:px-0 w-[325px] lg:w-[500px]">
+                <main className="px-4 sm:px-6 lg:flex-auto lg:px-0">
                     <div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
                         <div>
                             <AccountTitleComponent title={"Basic Profile Information"} desc={"Manage account and access basic information"}/>
@@ -25,7 +25,7 @@ export function ProfilePageComponent() {
                                     desc={user?.email} 
                                     buttonName="Logout"
                                 />
-                                {stripeRole !== 'premium' ? // If premium -> manage account; if free -> CTA
+                                {stripeRole == 'premium' ? // If premium -> manage account; if free -> CTA
                                 <SimpleProfileComponent
                                     onButtonClick={() => {window.open(`${process.env.NEXT_PUBLIC_STRIPE_NO_CODE_PORATL}`)}} 
                                     title={"Subscription"} 
