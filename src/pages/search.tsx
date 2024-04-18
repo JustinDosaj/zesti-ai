3fe7de884a8c2e766abad38b31200489fc4666e4
Justin Dosaj <jdosaj@vurge.io>
2024-04-18 16:12:52 -0700

    Fix

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -6,19 +6,10 @@ import { Raleway } from 'next/font/google'
 import { TitleSection } from '@/components/shared/title';
 import Breadcrumbs from '@/components/shared/breadcrumb';
 import useSetBreadcrumbs from '@/components/shared/setBreadcrumbs';
-import { AddModal } from '@/components/ui/modals/add';
-import { UserIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/router';
 import algoliasearch from 'algoliasearch/lite';
 
 const raleway = Raleway({subsets: ['latin']})
-
-interface Recipe {
-    objectID: string;
-    name: string;
-    cover_image_url: string;
-    // Define other properties as needed
-}
 
 const SearchResults: React.FC = () => {
     
@@ -28,10 +19,7 @@ const SearchResults: React.FC = () => {
     const recipesIndex = searchClient.initIndex(`${process.env.NEXT_PUBLIC_ALGOLIA_ALL_RECIPES_INDEX}`);
 
     const [ recipes, setRecipes ] = useState<any>([])
-    const [ isAddOpen, setIsAddOpen ] = useState<boolean>(false)
     const [ urlParam, setUrlParam ] = useState<string>('')
-
-    const [ url , setUrl ] = useState<string>('')
 
     const router = useRouter()
 
@@ -41,7 +29,6 @@ const SearchResults: React.FC = () => {
             if (q) {
                 setUrlParam(decodeURIComponent(q as string))
             }
-
             handleSearch(q)
         }
     },[router.isReady, router.query])
@@ -61,17 +48,8 @@ const SearchResults: React.FC = () => {
         <main className={`flex min-h-screen flex-col items-center bg-background w-screen space-y-4 pb-48 ${raleway.className}`}>
             <Breadcrumbs/>
             <TitleSection titleBlack="Search for Recipes" desc="Enter a TikTok Recipe link below or search by ingredients, instructions, usernames & more!"/>
-            {/*<Search searchLocation="my-recipes" setRecipes={setRecipes} showDropDown={false} initialQuery={urlParam}/>*/}
             <AddRecipe/>
             <RecipeCardList recipes={recipes}/>
-            <AddModal
-                isOpen={isAddOpen}
-                setIsOpen={setIsAddOpen}
-                title="Add Recipe"
-                text="Click share on the Tiktok video and select copy link. Then paste the link below to add the recipe!"
-                icon={UserIcon}
-            />
-
         </main>
     );
 }
