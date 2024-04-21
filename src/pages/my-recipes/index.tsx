af14e0be2b3a061b2975bf8ba46b33aa79d875c0
Justin Dosaj <jdosaj@vurge.io>
2024-04-21 14:52:08 -0700

    Fix

--- src/pages/my-recipes/index.tsx
+++ src/pages/my-recipes/index.tsx
@@ -33,7 +33,7 @@ export default function MyRecipes() {
         <RecipeCardList recipes={userRecipeList} maxDisplayCount={9} max={0} loading={loadingUserRecipes}/>
         {/* Ad Display for My Recipes */}
         <div className="pt-8"/>
-        <AdSenseDisplay adSlot="5606229053" adFormat="horizontal" widthRes="true" role={stripeRole}/>
+        <AdSenseDisplay adSlot="1979966274" adFormat="horizontal" widthRes="true" role={stripeRole}/>
     </main>
     </>
   )
