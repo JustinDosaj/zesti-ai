d6ef822cc32acd32df1a8dc64719dc8127b48907
Justin Dosaj <jdosaj@vurge.io>
2024-05-01 13:57:30 -0700

    Added admin function

--- src/pages/api/admin/functions.ts
+++ src/pages/api/admin/functions.ts
@@ -1,39 +1,19 @@
 import { Notify } from "@/components/shared/notify";
 import { getFunctions, httpsCallable } from 'firebase/functions';
-import { SendErrorToFirestore } from "../firebase/functions";
 
-interface ProtoTypeProps {
-  url?: any,
-  setUrl?: any,
-  user?: any,
+interface AdminProps {
+    recipeId?: string;
 }
 
-export async function AdminProtoTypeUserSubmit({url, setUrl, user}:ProtoTypeProps) {
-  var id;
-
-    if(url.includes('tiktok.com/t/')) {
-        id = url?.match(/^https:\/\/www\.tiktok\.com\/t\/([A-Za-z0-9_-]+)\/?$/);
-    } else {
-        id = url?.match(/tiktok\.com\/@[^\/]+\/video\/(\d+)/);
-    }
-
-    // Check if URL is empty   
-    const date: Date = new Date();
+export async function AdminUpdateRecipe({recipeId}: AdminProps) {
     const functions = getFunctions();
-    const userAddRecipe = httpsCallable(functions, 'userAddRecipe');
+    const adminUpdateRecipe = httpsCallable(functions, 'adminUpdateRecipe');
 
-    const recipeObj = {
-        "url": `${url}`,
-        "id": id ? id[1] : null,
-        "source": "tiktok",
-        "date_added": date.toISOString(),
+    const userInput = {
+        "recipeId": recipeId,
     }
 
-    try {
-        const response = await userAddRecipe(recipeObj)
-        Notify("Successfully Added Recipe")
-    } catch(err) {
-        Notify(`Error: ${err}`)
-        await SendErrorToFirestore(user?.uid, err)
-    }
+    await adminUpdateRecipe(userInput).then(() => Notify("Finished"))
+
+    return { recipeId }
 }
