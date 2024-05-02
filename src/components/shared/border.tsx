69343d73c52dfee4b8bb93e9ef0e5ddcc5847ead
Justin Dosaj <jdosaj@vurge.io>
2024-05-02 13:27:53 -0700

    Big Recipe Page Update
    Added the following:
    - Share button
    - Video ID
    - Zesti ID
    - Created Date
    - Date added to Zesti
    - source
    - link to owner profile
    - video title
    - video cover image
    
    Redesigned the following:
    - Recipe title -> now has share and save button positioned on bottom of section
    - Made text colors and sizes more uniform across all sections

--- src/components/shared/border.tsx
+++ src/components/shared/border.tsx
@@ -7,7 +7,7 @@ interface BorderProps {
 
 export function HorizontalBorder({className, width}: BorderProps) {
     return (
-        <div className={`border-t border-gray-300 m-12 ${className}`} style={{ width: width }} />
+        <div className={`border-t border-gray-400 border-opacity-75 ${className}`} style={{ width: width }} />
     )
 
 }
--- src/components/ui/recipe/card.tsx
+++ src/components/ui/recipe/card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
+import { Loader } from "@/components/shared/loader";
 
 interface RecipeCardProps {
     item: {
