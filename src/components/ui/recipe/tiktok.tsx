7bc7830ce59ce8680138eee82a9722680508cc69
Justin Dosaj <jdosaj@vurge.io>
2024-05-06 20:04:05 -0700

    Optimization Update

---
+++ src/components/ui/recipe/tiktok.tsx
@@ -0,0 +1,20 @@
+interface TikTokProps {
+    video_id: any,
+}
+
+const TikTokComponent = ({video_id}: TikTokProps) => {
+    return(
+        <div className="rounded-xl overflow-hidden alternate-orange-bg w-fit mx-auto h-full">
+
+        <iframe
+            className="w-full max-w-[325px] min-w-[325px] min-h-[800px]"
+            src={`https://www.tiktok.com/embed/${video_id}`}
+            allow="fullscreen"
+            loading="lazy"
+        />
+
+    </div>
+    )
+}
+
+export default TikTokComponent
