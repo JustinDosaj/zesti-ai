f3b97500e2a11cd541b1e526a84333df300c9df8
Justin Dosaj <jdosaj@vurge.io>
2024-07-10 16:56:44 -0700

    F

--- src/pages/explore/post/[slug].tsx
+++ src/pages/explore/post/[slug].tsx
@@ -48,8 +48,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const url = `${protocol}://${host}${resolvedUrl}`;
   
   const post = await getBlogPostBySlug(slug)
-  const relatedRecipeIds: string[] = Array.isArray(post?.fields.relatedRecipes) && post.fields.relatedRecipes.every(item => typeof item === 'string')
-    ? post.fields.relatedRecipes as string[]
+  const relatedRecipeIds: string[] = Array.isArray(post?.fields.relatedRecipes) && post?.fields.relatedRecipes.every(item => typeof item === 'string')
+    ? post?.fields.relatedRecipes as string[]
     : ['9dPr4zecWHggW81lZcfu', 'BIruI1eRcr5g4lZUVmZr', 'uSAW740E9cg3q6iI9hf7', 'Z5Gz54RTo978H81odNZf'];
 
   //['9dPr4zecWHggW81lZcfu', 'BIruI1eRcr5g4lZUVmZr', 'uSAW740E9cg3q6iI9hf7', 'Z5Gz54RTo978H81odNZf']
