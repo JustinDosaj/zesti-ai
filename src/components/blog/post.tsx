aa23ef50e5c107b5516fb016cf34b906fe63c6ed
Justin Dosaj <jdosaj@vurge.io>
2024-07-11 12:53:40 -0700

    Blog Update

--- src/components/blog/post.tsx
+++ src/components/blog/post.tsx
@@ -13,15 +13,15 @@ export const PostTitle = ({ title, author, date, description }: PostProps) => {
     
     return(
         <div className="relative flex flex-col text-left space-y-4">
-            <div className="items-center inline-flex mt-2">
-                <h1 className="text-4xl/tight sm:text-4xl/tight md:text-5xl/tight xl:text-5xl/tight font-semibold text-heading-1 text-gray-700 capitalize">
+            <div className="items-center inline-flex mt-2 prose">
+                <h1 className="text-4xl/tight sm:text-4xl/tight md:text-5xl/tight xl:text-5xl/tight font-bold text-heading-1 text-gray-700 capitalize">
                     <span>{title}</span> 
                 </h1>
             </div>
-            <Paragraph className="mt-2 text-gray-600">
+            <div className="mt-2 text-gray-800 prose">
                 {documentToReactComponents(description)}
-            </Paragraph>
-            <Paragraph className="text-gray-500 mt-2 mb-2 text-left text-sm">
+            </div>
+            <Paragraph className="text-gray-500 mt-2 mb-2 text-left prose">
                 <span className="text-sm">By </span>
                 <Link href="/about/author" className="underline hover:text-gray-600 text-sm">{author} Team</Link> 
                 <span className="text-sm"> | Updated on {date} </span>
