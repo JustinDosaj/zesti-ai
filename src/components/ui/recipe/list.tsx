fa09b18f5ab8b7bfd07e3ee1082e62794af8b532
Justin Dosaj <jdosaj@vurge.io>
2024-08-12 09:41:28 -0700

    Id Updates

--- src/components/ui/recipe/list.tsx
+++ src/components/ui/recipe/list.tsx
@@ -42,7 +42,7 @@ export function RecipeCardList({recipes, maxDisplayCount = 9, incrementCount = 9
                 <div className="space-y-6">
                     <div ref={containerRef} className={`grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-4`} >
                         {recipes.slice(0, displayCount).map((item: any) => (
-                            <RecipeCard item={item} key={item.data.unique_id}/>
+                            <RecipeCard item={item} key={item.data.id}/>
                         ))}
                     </div>
                       {shouldShowLoadMore && (
