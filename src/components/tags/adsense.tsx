535ecf95e7fc18a38aedc619bed88f617cf54844
Justin Dosaj <jdosaj@vurge.io>
2024-08-14 19:11:18 -0700

    REfix

--- src/components/tags/adsense.tsx
+++ src/components/tags/adsense.tsx
@@ -32,7 +32,7 @@ const AdSense: React.FC<AdSenseProps> = ({ adSlot, adFormat, className, adStyle 
   return (
     <div className={` ${className}`} style={{ textAlign: 'center', ...adStyle }}>
       <ins
-        className="bg-gray-300 adsbygoogle"
+        className="adsbygoogle"
         style={{ display: 'block', ...adStyle }}
         data-ad-client="ca-pub-5837655994202747"  // Replace with your AdSense Publisher ID
         data-ad-slot={adSlot}
