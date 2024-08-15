79125a08e18c6489f9780aa19a39b97d9539696c
Justin Dosaj <jdosaj@vurge.io>
2024-08-15 11:25:18 -0700

    Update Ads Again

--- src/components/ui/recipe/suggestions.tsx
+++ src/components/ui/recipe/suggestions.tsx
@@ -11,7 +11,7 @@ interface Suggestions {
 export function RecipeSuggestions({recipes, role, title}: Suggestions) { 
     return(
         <div className="mx-auto max-w-7xl w-full space-y-8 mt-6 lg:mt-24 flex min-h-screen flex-col items-center">
-            <AdSense className="max-w-5xl" adSlot="7480590418" adFormat="auto" adStyle={{ width: '100%', maxHeight: '90px' }} role={role}/>
+            <AdSense className="max-w-5xl" adSlot="7480590418" adFormat="horizontal" adStyle={{ width: '100%', maxHeight: '90px' }} role={role}/>
             <HorizontalBorder width="100%"/>
             <h2 className="text-gray-900 max-w-[1600px] w-full font-bold prose-2xl">{title}</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-3">
@@ -20,7 +20,6 @@ export function RecipeSuggestions({recipes, role, title}: Suggestions) {
               ))}
             </div>
             <AdSense className="max-w-5xl" adSlot="5275868942" adFormat="auto" adStyle={{ width: '100%', maxHeight: '320px' }} role={role}/>
-      
         </div>
 
     )
