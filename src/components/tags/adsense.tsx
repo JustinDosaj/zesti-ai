fa86c0ba2a99c8fdf80251d81da239a7460d0e6a
Justin Dosaj <jdosaj@vurge.io>
2024-08-14 19:51:02 -0700

    Add Updates

--- src/components/tags/adsense.tsx
+++ src/components/tags/adsense.tsx
@@ -31,12 +31,12 @@ const AdSense: React.FC<AdSenseProps> = ({ adSlot, adFormat, className, adStyle 
     }
   }, []);
   
-  if (role == 'premium') return null;
+  if (role !== 'premium') return null;
 
   return (
-    <div className={` ${className}`} style={{ textAlign: 'center', ...adStyle }}>
+    <div className={`${className}`} style={{ textAlign: 'center', ...adStyle }}>
       <ins
-        className="adsbygoogle"
+        className="bg-gray-600 adsbygoogle"
         style={{ display: 'block', ...adStyle }}
         data-ad-client="ca-pub-5837655994202747"  // Replace with your AdSense Publisher ID
         data-ad-slot={adSlot}
