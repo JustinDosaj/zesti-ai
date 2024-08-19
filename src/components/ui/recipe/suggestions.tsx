de9adf63d0317f2febc87ebe293ff695516c9497
Justin Dosaj <jdosaj@vurge.io>
2024-08-19 13:23:41 -0700

    Actual Last Fix For Day

--- src/components/ui/recipe/suggestions.tsx
+++ src/components/ui/recipe/suggestions.tsx
@@ -10,7 +10,7 @@ interface Suggestions {
 
 export function RecipeSuggestions({recipes, role, title}: Suggestions) { 
     return(
-        <div className="mx-auto max-w-7xl w-full space-y-8 mt-4 lg:mt-24 flex min-h-screen flex-col items-center">
+        <div className="mx-auto max-w-7xl w-full space-y-8 mt-8 lg:mt-24 flex min-h-screen flex-col items-center">
             <AdSense className="mx-auto max-w-[320px] md:max-w-full" adSlot="7480590418" adFormat="horizontal" adStyle={{ width: '100%', height: '100px', maxHeight: '320px' }} role={role}/>
             <HorizontalBorder width="100%"/>
             <h2 className="text-gray-900 max-w-[1600px] w-full font-bold prose-2xl">{title}</h2>
