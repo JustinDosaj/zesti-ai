a4bf7d31ca8a42eddcd156698eb0070907707f7a
Justin Dosaj <jdosaj@vurge.io>
2024-08-22 09:42:54 -0700

    Fix Pricing Title

--- src/pages/about/pricing.tsx
+++ src/pages/about/pricing.tsx
@@ -16,7 +16,7 @@ export default function Pricing() {
     <main className={`flex min-h-screen flex-col items-center bg-background w-full pb-28`}>
       <div className="mt-2 lg:mt-10"/>
       <Container className={"flex flex-col mx-auto bg-white"}>
-        <Title className="text-center">Pricing Plans</Title>
+        <Title className="text-center mt-4 md:mt-0">Pricing Plans</Title>
         <Paragraph className="mt-2 text-center">Join hundreds of users creating their favorite home cooked meals!</Paragraph>
         <p className="mx-auto sm:mb-8 mt-4 sm:mt-6 max-w-xl w-fit pr-3 pl-3 text-center leading-8 border border-primary-main rounded-3xl text-gray-600">
         Try for Free. Cancel anytime.
