b3d8a04a86611962674ca071d8590620f73c26c0
Justin Dosaj <jdosaj@vurge.io>
2024-08-25 18:21:31 -0700

    UI Updates

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,12 +1,8 @@
 // pages/search-results.tsx
-import { useEffect, useState } from 'react';
 import { SearchOrAddRecipe } from '@/components/search/search-add-recipe';
 import { Title } from '@/components/shared/title';
-import { useRouter } from 'next/router';
 import { useAuth } from '@/context/AuthContext';
 import Head from 'next/head';
-import algoliasearch, { SearchIndex } from 'algoliasearch';
-import { Notify } from '@/components/shared/notify';
 import AdSense from '@/components/tags/adsense';
 import { Paragraph } from '@/components/shared/paragraph';
 import { Container } from '@/components/shared/container';
@@ -27,52 +23,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 const AddRecipe: React.FC = ({recentRecipes}: any) => {
     
-    const searchClient = algoliasearch(`${process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID}`, `${process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY}`);
-    const recipesIndex: SearchIndex = searchClient.initIndex(`${process.env.NEXT_PUBLIC_ALGOLIA_ALL_RECIPES_INDEX}`);
-
-    recipesIndex.setSettings({
-        searchableAttributes: [
-            'unordered(name)',
-            'unordered(video_title)',
-            'unordered(description)',
-            'unordered(ingredients)'
-        ],
-        typoTolerance: 'true',
-        removeWordsIfNoResults: 'allOptional',
-    })
-
-    const [ recipes, setRecipes ] = useState<any>([])
     const { stripeRole } = useAuth();
-
-    const router = useRouter()
-
-    useEffect(() => {
-        if(router.isReady) {
-            const { q } = router.query
-            handleSearch(q)
-        }
-    },[router.isReady, router.query])
-
-    const handleSearch = async (query: any) => {
-
-        try {
-            const [ recipes ] = await Promise.all([
-                recipesIndex.search(query)
-            ]);
-
-            const sortedRecipes = recipes.hits.sort((a: any, b: any) => {
-                const dateA = new Date(a.data.date_added).getTime();
-                const dateB = new Date(b.data.date_added).getTime();
-                return dateB - dateA;
-            });
-
-            setRecipes(sortedRecipes) 
-            
-        } catch(error) {
-            Notify("Problem fetching recipes, please try again later")
-        }
-
-    };
 
     return (
         <>
@@ -87,7 +38,7 @@ const AddRecipe: React.FC = ({recentRecipes}: any) => {
                     <Title className="text-center">Add Recipe</Title>
                     <Paragraph className="mt-2 text-center">Copy & paste a TikTok or Instagram link to save the recipe in seconds!</Paragraph>
                 </Container>
-                <SearchOrAddRecipe align={"center"}/>
+                <SearchOrAddRecipe/>
                 <div className="pt-24"/>
                 <Gallery recipes={recentRecipes}/>
                 <div className="mt-2"/>
