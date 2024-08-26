8a37b31165b5a6e7d58b38ac30ada7833563fd00
Justin Dosaj <jdosaj@vurge.io>
2024-08-26 11:08:20 -0700

    Slight UI Update - Ads

--- src/components/ui/general/recipe-suggestions.tsx
+++ src/components/ui/general/recipe-suggestions.tsx
@@ -11,7 +11,7 @@ interface Suggestions {
 export function RecipeSuggestions({recipes, role, title}: Suggestions) { 
     return(
         <div className="mx-auto max-w-7xl w-full space-y-8 mt-8 lg:mt-24 flex min-h-screen flex-col items-center">
-            <AdSense className="mx-auto max-w-[320px] md:max-w-full" adSlot="7480590418" adFormat="horizontal" adStyle={{ width: '100%', height: '100px', maxHeight: '320px' }} role={role}/>
+            <AdSense className="mx-auto max-w-[320px] md:max-w-5xl" adSlot="7480590418" adFormat="horizontal" adStyle={{ width: '100%', height: '90px', maxHeight: '90px' }} role={role}/>
             <HorizontalBorder width="100%"/>
             <h2 className="text-3xl font-semibold mb-4 text-gray-800 max-w-[1600px] w-full">{title}</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-3">
@@ -19,7 +19,7 @@ export function RecipeSuggestions({recipes, role, title}: Suggestions) {
                 <RecipeCard key={recipe.id} item={recipe} />
               ))}
             </div>
-            <AdSense className="max-w-5xl" adSlot="5275868942" adFormat="auto" adStyle={{ width: '100%', maxHeight: '320px' }} role={role}/>
+            <AdSense className="mx-auto max-w-[320px] md:max-w-5xl" adSlot="5275868942" adFormat="auto" adStyle={{ width: '100%', height: '320px', maxHeight: '320px' }} role={role}/>
         </div>
 
     )
