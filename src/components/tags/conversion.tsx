5bdf132883ccd8b12a248da1c0d7887640b13988
Justin Dosaj <jdosaj@vurge.io>
2024-09-09 11:11:54 -0700

    Fix

--- src/components/tags/conversion.tsx
+++ src/components/tags/conversion.tsx
@@ -3,7 +3,7 @@ export default function GoogleTags() {
     return(
     <>
         <meta name="google-adsense-account" content="ca-pub-5837655994202747"></meta>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBIC_GOOGLE_CONVERSION_ID}`}></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_CONVERSION_ID}`}></script>
         <meta name="google-adsense-account" content="ca-pub-5837655994202747"></meta>
         <script dangerouslySetInnerHTML={{
           __html: `
@@ -11,7 +11,7 @@ export default function GoogleTags() {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', '${process.env.NEXT_PUBIC_GOOGLE_CONVERSION_ID}');
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_CONVERSION_ID}');
           `
         }} />
     </>
